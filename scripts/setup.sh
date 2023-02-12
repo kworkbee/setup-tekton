33a0c2c14ec0aa6372236138e0e878e63176caf0
전지원(Jiwon Jeon)/Core플랫폼개발팀/11ST <jiwon_jeon@sk.com>
2023-02-12 13:49:25 +0900

    feat: Initial Commit

---
+++ scripts/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+###################
+## Prerequisites ##
+###################
+# minikube
+# cri-tools
+
+# 1. Start local cluster (using minikube)
+minikube start --kubernetes-version=v1.24.0 --force-systemd
+
+# 2. Check minikube cluster started
+kubectl cluster-info
+
+# 3. Install Tekton Pipeline
+TEKTON_PIPELINE_VERSION=v0.41.1
+kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/previous/$TEKTON_PIPELINE_VERSION/release.yaml
+
+# 4. Check whether Tekton Ready
+kubectl -n tekton-pipelines wait --for=condition=Ready --timeout=1200s -l app=tekton-pipelines-controller pods
+
+# 5. Label
+kubectl label --overwrite ns tekton-pipelines pod-security.kubernetes.io/enforce=privileged
+
+# 6. Enable Web UI
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/release-full.yaml
+kubectl -n tekton-pipelines wait --for=condition=Ready --timeout=30s -l app=tekton-dashboard pods
+kubectl --namespace tekton-pipelines port-forward svc/tekton-dashboard 9097:9097 &
+
+# 7. Install Tekton Triggers
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
